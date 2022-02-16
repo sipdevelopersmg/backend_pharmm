e284a64b4222bbfdd5f2874e233d0cc37811ddd8
Yusron Nasrullah <yusronn@gmail.com>
2022-02-16 11:48:37 +0700

    healthy check

---
+++ Pharmm.API/Controllers/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pharmm.API.Controllers
+{
+    [ApiController]
+    [Route("[action]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> health()
+        {
+            try
+            {
+                return Ok("Healthy");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
